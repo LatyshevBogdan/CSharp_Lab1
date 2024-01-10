2e27b697d8d3fa2a54a97dae9317f417530dc5dd
LatyshevBogdan <126398794+LatyshevBogdan@users.noreply.github.com>
2024-01-10 18:41:34 +0300

    Add files via upload

---
+++ Lab1/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bebdb7df-a256-477a-a297-39341d2dbd0b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lab1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Part2;
+using Part3;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double sideA=11.1, sideB=12.1, answer=134.31;
+
+            Rectangle obj1 = new Rectangle(sideA, sideB);
+
+            double exept = obj1.Area;
+
+            Assert.AreEqual(answer, exept);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double sideA = 11.1, sideB = 12.1, answer = 46.4;
+
+            Rectangle obj1 = new Rectangle(sideA, sideB);
+
+            double exept = obj1.Perimeter;
+
+            Assert.AreEqual(answer, exept);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Point p1 = new Point(1, 2);
+            Point p2 = new Point(10, 2);
+            Point p3 = new Point(0, 2);
+            Figure f1 = new Figure(p1, p2, p3);
+            double ans = 9;
+            double expect = f1.LengthSide(p1, p2);
+
+
+            Assert.AreEqual(ans, expect);
+        }
+    }
+}
